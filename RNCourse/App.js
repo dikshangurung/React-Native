82491a6327818e0bda975d5ecd4d9d30d27da29b
dikshan gurung <dxngrg2058@gmail.com>
2023-11-24 20:31:31 +0545

    first commit

--- RNCourse/App.js
+++ RNCourse/App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
 	return (
-		<View style={styles.container}>
-			<Text>Hello</Text>
-			<StatusBar style="auto" />
+		<View style={styles.appContainer}>
+			<View style={styles.inputContainer}>
+				<TextInput
+					style={styles.textInput}
+					placeholder="Your course goal"
+				/>
+				<Button title="Add Goal" />
+			</View>
+			<View sty>
+				<Text>List of goals...</Text>
+			</View>
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
-	container: {
+	appContainer: {
+		paddingTop: 50,
+		paddingHorizontal: 16,
+	},
+	inputContainer: {
 		flex: 1,
-		backgroundColor: "#fff",
+		flexDirection: "row",
+		justifyContent: "space-between",
 		alignItems: "center",
-		justifyContent: "center",
+		paddingBottom: 24,
+		borderBottomColor: "#cccccc",
+		borderBottomWidth: 1,
+	},
+	textInput: {
+		width: "70%",
+		borderColor: "#cccccc",
+		borderWidth: 1,
+		padding: 8,
+		marginRight: 8,
+	},
+	goalsContainer: {
+		flex: 3,
 	},
 });
